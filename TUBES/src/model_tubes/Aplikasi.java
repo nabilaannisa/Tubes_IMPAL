2e333d399db558be1a19bb77eb9ab9e77029fc94
nabilaannisa <nabilaannisa.6b08@gmail.com>
2017-11-20 22:51:35 +0700

    update sementara aplikasi.java

--- TUBES/src/model_tubes/Aplikasi.java
+++ TUBES/src/model_tubes/Aplikasi.java
@@ -5,6 +5,7 @@
  */
 package model_tubes;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 /**
@@ -12,116 +13,170 @@ import java.util.ArrayList;
  * @author personal
  */
 public class Aplikasi {
-    private ArrayList<Bahan> daftarBahan;
-    private ArrayList<Supplier> daftarSupplier;
-    private ArrayList<Pegawai> daftarPegawai;
-    private ArrayList<Customer> daftarCustomer;
-    private ArrayList<Transaksi> Transaksi;
     
-    public Aplikasi() {
-        daftarBahan = new ArrayList<>();
-        daftarSupplier = new ArrayList<>();
-        daftarPegawai = new ArrayList<>();
-        daftarCustomer = new ArrayList<>();
-        Transaksi = new ArrayList<>();
+    static Pegawai pgw;
+
+    public static Pegawai getPgw() {
+        return pgw;
     }
-    //--------------------------Bahan--------------------------------
-    
-    public ArrayList<Bahan> daftarBahan() {
-        return daftarBahan;
+
+    public static void setPgw(Pegawai p) {
+        Aplikasi.pgw = p;
     }
     
-    public Bahan getBahan(int i) {
-        return daftarBahan.get(i);
-    }
+//    public void savePegawai(Pegawai p){
+//        Database db = new Database();
+//        String s = "insert into pegawai values ('" + pgw.getIdPegawai() + "','" + pgw.getNamaPegawai() + "','" + pgw.getAlamat() + "','" 
+//                + pgw.getNoHP() + "','" + pgw.getUsername() + "','" + pgw.getPass()+ "')" ;
+//        db.query(s);
+//    }
     
-    public String[] getDaftarBahan() {
-        String[] daftarID = new String[daftarBahan.size()];
-        for (int i=0; i<daftarID.length; i++) {
-            daftarID[i] = daftarBahan.get(i).getKodeBahan();
+    public Pegawai getPegawai(){
+        Database db = new Database();
+        Pegawai p = null;
+        ResultSet rs;
+        try {
+            String s = "select*from pegawai" ;
+            rs = db.getData(s);
+            while(rs.next()){
+                p = new Pegawai(rs.getString(1), rs.getString(2));
+                p.setUsername(rs.getString(3));
+                p.setPwdPegawai(rs.getString(4));
+            }   
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
-        return daftarID;
+        return p;
     }
-    
-    public void tambahBahan(Bahan barang) {
-        daftarBahan.add(barang);
-    }
-    
-    public Bahan getBahanDariNama(String nama) {
-        Bahan s = null;
-        String[] daftarBahanID = new String[daftarBahan.size()];
-        for (int j = 0; j<daftarBahanID.length; j++) {
-            if (nama == daftarBahan.get(j).getNamaBahan()) {
-                s = daftarBahan.get(j);
-            }
+    public Pegawai getPegawaiUser(String user, String pass){
+        Database db = new Database();
+        Pegawai p = null;
+        ResultSet rs;
+        try {
+            String s = "select*from pegawai where username = '" + user + "' and password= '" + pass + "'" ;
+            rs = db.getData(s);
+            while(rs.next()){
+                p = new Pegawai(rs.getString(1), rs.getString(2));
+                p.setUsername(rs.getString(3));
+                p.setPwdPegawai(rs.getString(4));
+            }   
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
-        return s;
+        return p;
     }
-    
-    //--------------------------Pegawai--------------------------------
-   
-    public String[] getDaftarPegawai() {
-        String[] daftarID = new String[daftarPegawai.size()];
-        for (int i=0; i<daftarID.length; i++) {
-            daftarID[i] = daftarPegawai.get(i).getIdPegawai();
-        }
-        return daftarID;
-    }
-    
-    public ArrayList<Pegawai> daftarPegawai() {
-        return daftarPegawai;
-    }
-    
-    public void tambahPegawai(Pegawai staff) {
-        daftarPegawai.add(staff);
-    }
-    
-    public Pegawai getPegawai(int i) {
-        return daftarPegawai.get(i);
-    }
-    
-    public Pegawai getPegawaiDariID(String id) {
-        Pegawai s = null;
-        String[] daftarPegawaiID = new String[daftarPegawai.size()];
-        for (int j = 0; j<daftarPegawaiID.length; j++) {
-            if (id == daftarPegawai.get(j).getIdPegawai()) {
-                s = daftarPegawai.get(j);
-            }
-        }
-        return s;
-    }
-    //--------------------------Customer--------------------------------
-    
-    public String[] getDaftarCustomer() {
-        String[] daftarID = new String[daftarCustomer.size()];
-        for (int i=0; i<daftarID.length; i++) {
-            daftarID[i] = daftarCustomer.get(i).getIdCustomer();
-        }
-        return daftarID;
-    }
-    
-    public ArrayList<Customer> daftarCustomer() {
-        return daftarCustomer;
-    }
-    
-    public Customer getCustomer(int i) {
-        return daftarCustomer.get(i);
-    }
-    
-    public void tambahCustomer(Customer penyewa) {
-        daftarCustomer.add(penyewa);
-    }
-    
-    public Customer getCustomerDariID(String id) {
-        Customer s = null;
-        String[] daftarCustomerID = new String[daftarCustomer.size()];
-        for (int j = 0; j<daftarCustomerID.length; j++) {
-            if (id == daftarCustomer.get(j).getIdCustomer()) {
-                s = daftarCustomer.get(j);
-            }
-        }
-        return s;
-    }
-    
-    
+//    private ArrayList<Bahan> daftarBahan;
+//    private ArrayList<Supplier> daftarSupplier;
+//    private ArrayList<Pegawai> daftarPegawai;
+//    private ArrayList<Customer> daftarCustomer;
+//    private ArrayList<Transaksi> Transaksi;
+//    
+//    public Aplikasi() {
+//        daftarBahan = new ArrayList<>();
+//        daftarSupplier = new ArrayList<>();
+//        daftarPegawai = new ArrayList<>();
+//        daftarCustomer = new ArrayList<>();
+//        Transaksi = new ArrayList<>();
+//    }
+//    //--------------------------Bahan--------------------------------
+//    
+//    public ArrayList<Bahan> daftarBahan() {
+//        return daftarBahan;
+//    }
+//    
+//    public Bahan getBahan(int i) {
+//        return daftarBahan.get(i);
+//    }
+//    
+//    public String[] getDaftarBahan() {
+//        String[] daftarID = new String[daftarBahan.size()];
+//        for (int i=0; i<daftarID.length; i++) {
+//            daftarID[i] = daftarBahan.get(i).getKodeBahan();
+//        }
+//        return daftarID;
+//    }
+//    
+//    public void tambahBahan(Bahan barang) {
+//        daftarBahan.add(barang);
+//    }
+//    
+//    public Bahan getBahanDariNama(String nama) {
+//        Bahan s = null;
+//        String[] daftarBahanID = new String[daftarBahan.size()];
+//        for (int j = 0; j<daftarBahanID.length; j++) {
+//            if (nama == daftarBahan.get(j).getNamaBahan()) {
+//                s = daftarBahan.get(j);
+//            }
+//        }
+//        return s;
+//    }
+//    
+//    //--------------------------Pegawai--------------------------------
+//   
+//    public String[] getDaftarPegawai() {
+//        String[] daftarID = new String[daftarPegawai.size()];
+//        for (int i=0; i<daftarID.length; i++) {
+//            daftarID[i] = daftarPegawai.get(i).getIdPegawai();
+//        }
+//        return daftarID;
+//    }
+//    
+//    public ArrayList<Pegawai> daftarPegawai() {
+//        return daftarPegawai;
+//    }
+//    
+//    public void tambahPegawai(Pegawai staff) {
+//        daftarPegawai.add(staff);
+//    }
+//    
+//    public Pegawai getPegawai(int i) {
+//        return daftarPegawai.get(i);
+//    }
+//    
+//    public Pegawai getPegawaiDariID(String id) {
+//        Pegawai s = null;
+//        String[] daftarPegawaiID = new String[daftarPegawai.size()];
+//        for (int j = 0; j<daftarPegawaiID.length; j++) {
+//            if (id == daftarPegawai.get(j).getIdPegawai()) {
+//                s = daftarPegawai.get(j);
+//            }
+//        }
+//        return s;
+//    }
+//    //--------------------------Customer--------------------------------
+//    
+//    public String[] getDaftarCustomer() {
+//        String[] daftarID = new String[daftarCustomer.size()];
+//        for (int i=0; i<daftarID.length; i++) {
+//            daftarID[i] = daftarCustomer.get(i).getIdCustomer();
+//        }
+//        return daftarID;
+//    }
+//    
+//    public ArrayList<Customer> daftarCustomer() {
+//        return daftarCustomer;
+//    }
+//    
+//    public Customer getCustomer(int i) {
+//        return daftarCustomer.get(i);
+//    }
+//    
+//    public void tambahCustomer(Customer penyewa) {
+//        daftarCustomer.add(penyewa);
+//    }
+//    
+//    public Customer getCustomerDariID(String id) {
+//        Customer s = null;
+//        String[] daftarCustomerID = new String[daftarCustomer.size()];
+//        for (int j = 0; j<daftarCustomerID.length; j++) {
+//            if (id == daftarCustomer.get(j).getIdCustomer()) {
+//                s = daftarCustomer.get(j);
+//            }
+//        }
+//        return s;
+//    }
+//    
+//    
 }
